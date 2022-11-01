c05dbb7028a04be9b4af77b0d68bec0d5bd3c063
Laxman-gaur <113633579+Laxman-gaur@users.noreply.github.com>
2022-11-01 10:46:27 +0530

    Update node-hello.js

--- node-hello.js
+++ node-hello.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'welcome to node js apploication!\n'
+  const msg = 'welcome to node js !\n'
   res.end(msg);
 });
 
